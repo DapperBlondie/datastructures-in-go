69d91ca8f74deedd339c548d7b0a0d3c5b1ea814
DapperBlondie <alirezagharib110@gmail.com>
2021-08-08 01:56:51 +0430

    Reformatting LinkedList pkg

---
+++ src/linkedlist/types.go
@@ -0,0 +1,16 @@
+package linkedlist
+
+type LinkedList interface {
+	AddToHead(data *struct{}) *Node
+	IterateAllOver()
+	SpecificNode(i uint) (*Node, error)
+	LastNode() *Node
+	AddToEnd(data *struct{})
+	NodeWithData(data *struct{}) (*Node, error)
+	AddAfterSpecificNode(i uint, data *struct{}) error
+}
+
+type Node struct {
+	Data *struct{}
+	Next *Node
+}
