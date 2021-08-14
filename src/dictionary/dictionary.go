d4f51f5073d6b9391c4ff1e3a61aed40fbfe90d0
DapperBlondie <alirezagharib110@gmail.com>
2021-08-15 00:40:33 +0430

    Dictionary is initialized

--- src/dictionary/dictionary.go
+++ src/dictionary/dictionary.go
@@ -1,1 +1,27 @@
 package dictionary
+
+import (
+	"github.com/rs/zerolog/log"
+	"sync"
+)
+
+func NewDict() *Dict {
+	dict := &Dict{
+		E: make(map[Key]Value),
+		M: &sync.RWMutex{},
+	}
+
+	return dict
+}
+
+func (d *Dict) Put(k Key, v Value) {
+	d.M.Lock()
+	defer d.M.Unlock()
+	if d.E == nil {
+		log.Error().Msg("your dictionary is not initialized yet")
+		return
+	}
+
+	d.E[k] = v
+	return
+}
--- src/dictionary/type.go
+++ src/dictionary/type.go
@@ -1,1 +1,16 @@
 package dictionary
+
+import "sync"
+
+type Key interface{}
+
+type Value *struct{}
+
+type Dict struct {
+	E map[Key]Value
+	M *sync.RWMutex
+}
+
+type Dictionary interface {
+	Put(k Key, v Value)
+}
