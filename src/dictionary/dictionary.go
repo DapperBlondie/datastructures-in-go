6448a23a6878ac38a26c3f05cd36e709961fa117
DapperBlondie <alirezagharib110@gmail.com>
2021-08-15 13:37:06 +0430

    Dictionary function is implemented

--- src/dictionary/dictionary.go
+++ src/dictionary/dictionary.go
@@ -25,3 +25,64 @@ func (d *Dict) Put(k Key, v Value) {
 	d.E[k] = v
 	return
 }
+
+func (d *Dict) Remove(k Key) bool {
+	d.M.Lock()
+	defer d.M.Unlock()
+	_, exists := d.E[k]
+	if exists {
+		delete(d.E, k)
+		return true
+	}
+
+	return false
+}
+
+func (d *Dict) Contains(k Key) bool {
+	d.M.RLock()
+	defer d.M.RUnlock()
+	_, exists := d.E[k]
+
+	return exists
+}
+
+func (d *Dict) FindByKey(k Key) Value {
+	d.M.RLock()
+	defer d.M.RUnlock()
+	return d.E[k]
+}
+
+func (d *Dict) Reset() {
+	d.M.Lock()
+	defer d.M.Unlock()
+	d.E = make(map[Key]Value)
+	return
+}
+
+func (d *Dict) NumberOfElements() int {
+	d.M.RLock()
+	defer d.M.RUnlock()
+	return len(d.E)
+}
+
+func (d *Dict) GetKeys() []Key {
+	d.M.RLock()
+	defer d.M.RUnlock()
+	var dictKeys []Key = []Key{}
+	for K := range d.E {
+		dictKeys = append(dictKeys, K)
+	}
+
+	return dictKeys
+}
+
+func (d *Dict) GetValues() []Value {
+	d.M.RLock()
+	defer d.M.RUnlock()
+	var dictValues []Value = []Value{}
+	for _, V := range d.E {
+		dictValues = append(dictValues, V)
+	}
+
+	return dictValues
+}
--- src/dictionary/type.go
+++ src/dictionary/type.go
@@ -2,8 +2,10 @@ package dictionary
 
 import "sync"
 
+// Key is a type that designed to hold any type of integer, string, float32 and float64 as a key in Dict
 type Key interface{}
 
+// Value is a type for holding your complex objects that a pointer to a struct
 type Value *struct{}
 
 type Dict struct {
@@ -13,4 +15,10 @@ type Dict struct {
 
 type Dictionary interface {
 	Put(k Key, v Value)
+	Remove(k Key) bool
+	Contains(k Key) bool
+	Reset()
+	NumberOfElements() int
+	GetKeys() []Key
+	GetValues() []Value
 }
